64c6ebc9414da4c1dbcbb2d543cda1904164c878
C4RC0 <karesz.carco@gmail.com>
2021-09-18 12:48:32 +0200

    initial release

--- packages/the-vase-and-the-finial/src/common/config/constants/menus.js
+++ packages/the-vase-and-the-finial/src/common/config/constants/menus.js
@@ -2,6 +2,7 @@ const menus = {
     conceptMenu: "The concept",
     pureCssMenu: "Pure css",
     gitHubMenu: "GitHub",
+    behanceMenu: "Behance",
 };
 
 export default menus;
--- packages/the-vase-and-the-finial/src/common/config/constants/routes.js
+++ packages/the-vase-and-the-finial/src/common/config/constants/routes.js
@@ -1,7 +1,8 @@
 const routes = {
     conceptRoute: "/concept",
     pureCssRoute: "/pure-css",
-    gitHubUrl: "https://github.com/C4RC0/the-vase-and-the-finial"
+    gitHubUrl: "https://github.com/C4RC0/the-vase-and-the-finial",
+    behanceUrl: "https://www.behance.net/gallery/127631653/The-vase-and-the-finial",
 };
 
 export default routes;
